d73dc923dad3e04a9ec98d65fe5db64f6e853309
Adeilton <i1iadeilton@gmail.com>
2022-12-06 20:35:46 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,6 @@
 package chess;
 
 import boardgame.Board;
-
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -29,8 +27,13 @@ public class ChessMatch {
 		return matriz;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		/*toPosition para converter para posição de matriz*/
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void inicialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.WHITE));
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,40 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+	public int row;
+	public char column;
+	public ChessPosition(char column, int row) {
+		if(row < 1 || row > 8 || column < 'a' || column > 'h') {
+			throw new ChessException("Error instantianting Chess Position. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public char getColumn() {
+		return column;
+	}
+		
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	protected ChessPosition fromPosition(Position position) {
+		/*Cast para char*/
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override 
+	public String toString() {
+		/*As aspas mostra para o compilador que é um concatenação de string*/
+		return "" + column + row;
+	}
+	
+}

a086d196202de439f45897c7247f141f11c019a9
Adeilton <i1iadeilton@gmail.com>
2022-12-08 12:20:00 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -54,6 +54,19 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	public boolean positionExists(int row, int column) {
 		/*rows e colums são a dimensão do tabuleiro*/
 		return row >= 0 && row < rows && column >= 0 && column < columns;
